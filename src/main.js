72b4ca748df4e1cfdf68f61eb2a20b45bec8dd76
Marifer <mfvl.1186@gmail.com>
2023-02-01 17:33:59 -0300

    faltante

--- src/main.js
+++ src/main.js
@@ -5,6 +5,22 @@
 //construirás una página web para visualizar un conjunto (set) de datos
 //se adecúe a lo que descubras que tu usuario necesita.
 
+
+// TODO:  Escoger fotos cartas reves
+// TODO:  Escoger fotos cartas derecho
+// TODO: Imagen Fondo 
+
+// TODO:  chequear  date input
+// TODO:  slice e invertir date
+// TODO:  Time Input 
+// TODO:  Lugar ==> link long/lat
+// TODO:  Time Input 
+
+
+
+
+
+
 // Usa VanillaJS.
 //visualizar la data ==>API ==> JSON,
 // TODO: conexión DOM ==> main.js
