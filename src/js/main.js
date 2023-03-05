3c10ec354f29dcb326bcc0c6d7b6dce09b42962b
Gaabs <ggaabrielagomez@gmail.com>
2023-03-05 00:28:02 -0600

    css responsive y api conectada

--- src/js/main.js
+++ src/js/main.js
@@ -131,9 +131,9 @@ function validateForm() {
   return valid;
 }
 
-/* const today = new Date().toLocaleString("sv-SE").replace(" ", "T").slice(0, 16);
+const today = new Date().toLocaleString("sv-SE").replace(" ", "T").slice(0, 16);
 const dob = document.getElementById("DateOB").value;
- */
+
 
 function fixStepIndicator(n) {
   let i;
@@ -187,53 +187,66 @@ function initMap() {
 
 window.initMap = initMap;
 
-/* let dateTime; */
+let dateTime;
 
 // Get time zone from users location
-/* async function getTimeZone() {
+async function getTimeZone() {
   const promise = new Promise((resolve) => {
     dateTime = document.getElementById("birthdaytime").value;
     const offset = new Date(dateTime).getTimezoneOffset(),
       o = Math.abs(offset);
     resolve(
       (offset < 0 ? "+" : "-") +
-        ("00" + Math.floor(o / 60)).slice(-2) +
-        ":" +
-        ("00" + (o % 60)).slice(-2)
+      ("00" + Math.floor(o / 60)).slice(-2) +
+      ":" +
+      ("00" + (o % 60)).slice(-2)
     );
-  }); 
+  });
 
-   const result = await promise;
+  const result = await promise;
   return result;
-} */
+}
+
 
 /* API SECTION */
 
-/* END API SECTION */
+const url = "https://api.prokerala.com/v2/astrology/birth-details";
+const data = {
+  ayanamsa: "1",
+  coordinates: latLong,
+  datetime: dateTime + ":00" + getTimeZone(),
+  la: "en",
+};
 
-const astroData = fetch("/data/astrology.json")
-  .then((response) => response.json())
-  .then((info) => {
-    return info.data;
-  })
-  .catch((error) => error);
+const myHeaders = new Headers();
+myHeaders.append(
+  "Authorization",
+  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI2MzljZWQ5OC1mYWNhLTQ3YTItOTk1ZC1jMGQwMzRmMjgyYTEiLCJqdGkiOiJjOWY1NzAxNmU5MTk4NWRjY2VhNzNhMzZiMWIyMjM1Zjc3ZjQ1YzYwYmZhZWJiMjgxN2Y1NTQyMTA2Yzk0M2ZjODY4ODVkNjRiMWUyMDQ3NSIsImlhdCI6MTY3NzAwMTYzNi43OTg1OSwibmJmIjoxNjc3MDAxNjM2Ljc5ODU5MywiZXhwIjoxNjc3MDA1MjM2Ljc5ODM3Niwic3ViIjoiMmQ3MDRmMDYtNjlhMC00OTdkLWI0YTQtOTE2OWZkZTk1YThlIiwic2NvcGVzIjpbXSwiY3JlZGl0c19yZW1haW5pbmciOjQ3MDAsInJhdGVfbGltaXRzIjpbeyJyYXRlIjo1LCJpbnRlcnZhbCI6NjB9XX0.HOmB6NTg6GaPYZXBgu-yej_FqF52KRvxgzSB0KcotnK4LaCOs-Rte4MHLjygwQNL6CUwCTDWQO6mJdg_cajNj9QGUPbvKb8jZAvSdIwpc_c-45r2q0J4BubLJWLl3KcDvE7sr_7JuKvZP7PhrPDv3j_PjEGjo0oc9fZqivJpXqtTXUI8gAnV5D7oAWN6FyJkQmVfgZ3WcF25RfJn2fSkM1zZQpuQ-Ej99_9_pcazaB6X-G-fLYhbm32k3jTtQx7hF0qvge5tsVtBSKT66izhcRqi_GbwLxh2SRYnn6KzCQJRxfVv4Oz-6OYokovkVztnHOpM1fh3nMczMPWzQ0a-bw"
+);
+
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
+
+const astroData = fetch(url + new URLSearchParams(data), requestOptions)
+  .then((res) => res.text()) // res.json()
+  .catch((error) => console.error("Error:", error))
+  .then((response) => console.log("Success:", response.json()));
 
 const dataAPI = async () => {
   const wait = await astroData;
   createCards(wait);
 };
 
-/* let zodiac;
-const printData = async () => {
-  const a = await astroData;
-  console.log("AstroData: " + astroData);
-  zodiac = a["zodiac"]["name"];
-  console.log("Zodiac dentro de astro " + zodiac);
-}; */
+
+
+/* END API SECTION */
+
 
 /* CARD SECTION */
-let zodiac;
-
+let zodiac
 // Create & print main results
 function createCards(data) {
   const button1 = document.getElementById("typeAyur");
@@ -260,10 +273,10 @@ function createCards(data) {
     button2.href = "https://indiaveda.com/p/equilibra-vata";
   } else if (nadi === "Pitta") {
     button1.href = "https://indiaveda.com/p/constitucion-pitta";
-    button2.href = "https://indiaveda.com/p/equilibra-pitta"; 
+    button2.href = "https://indiaveda.com/p/equilibra-pitta";
   } else if (nadi === "Kapha") {
     button1.href = "https://indiaveda.com/p/constitucion-kapha";
-    button2.href = "https://indiaveda.com/p/equilibra-kapha"; 
+    button2.href = "https://indiaveda.com/p/equilibra-kapha";
   }
 
 }
